91260fefca9d40f87ddd7aebf5cea7098c1c7f86
Jibachh Singh <84733181+JibachhhMahto764@users.noreply.github.com>
2024-10-05 15:51:12 +0545

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -12,4 +12,6 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 
 //admin panel
 Route::get('/admin/login', [App\Http\Controllers\Admin\AdminLogincontroller::class, 'index'])->name('admin.login');
+Route::get('/admin/registration', [App\Http\Controllers\Admin\AdminLogincontroller::class, 'registration'])->name('admin.registration');
+Route::get('/admin/logout', [App\Http\Controllers\Admin\AdminLogincontroller::class, 'index'])->name('admin.logout');
 
